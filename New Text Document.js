f70653b81daecc02ab6e2092e194830b71eada0e
chanuka-dissanayaka <chanukaati001@gmail.com>
2025-04-25 16:50:13 +0530

    Add sidebar toggle functionality

--- New Text Document.js
+++ New Text Document.js
@@ -27,4 +27,10 @@ themeToggle.addEventListener('click', () => {
         themeToggle.prepend(icon);
     }
 });
+const navToggler = document.querySelector(".nav-toggler");
+const aside = document.querySelector(".aside");
+
+navToggler.addEventListener("click", () => {
+    aside.classList.toggle("open");
+});
 
