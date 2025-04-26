74243c78884481083be8206d3ee665a528432956
chanuka-dissanayaka <chanukaati001@gmail.com>
2025-04-26 18:05:30 +0530

    Refactor sidebar toggle functionality and improve event handling

--- New Text Document.js
+++ New Text Document.js
@@ -27,10 +27,8 @@ themeToggle.addEventListener('click', () => {
         themeToggle.prepend(icon);
     }
 });
-const navToggler = document.querySelector(".nav-toggler");
-const aside = document.querySelector(".aside");
-
-navToggler.addEventListener("click", () => {
-    aside.classList.toggle("open");
-});
-
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("open");
+  }
+  
